35af2b898717f3ad7ed750a2c5efa7bb1904bec6
chenshijie <csjpro0810@163.com>
2021-12-02 18:27:18 +0800

    feat: 完善系统首页

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,17 @@
-const Index: React.FC = () => {
-  return <div>首页</div>;
+import React from 'react';
+import welcome from '@/assets/imgs/welcome.png';
+import style from './Home.less';
+
+const Welcome: React.FC = () => {
+  return (
+    <div className={style.container}>
+      <img src={welcome} alt="" />
+      <div className={style.right}>
+        <span className={style.title}>欢迎</span>
+        <span className={style.desc}>登录鑫业务系统~</span>
+      </div>
+    </div>
+  );
 };
-export default Index;
+
+export default Welcome;
