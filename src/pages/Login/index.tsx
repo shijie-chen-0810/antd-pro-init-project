19a2aa16336b60c8d6c0515e365501b111b8487e
chenshijie <csjpro0810@163.com>
2022-01-11 14:37:42 +0800

    feat: msg

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -75,7 +75,7 @@ const Login: React.FC = () => {
               fieldProps={{
                 size: 'large',
               }}
-              placeholder="密码: 随便输"
+              placeholder="密码: 随便输123"
               rules={[
                 {
                   required: true,
